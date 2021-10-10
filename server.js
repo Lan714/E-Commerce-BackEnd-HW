55e1d559ac42bbd8fc1f73edfb0eb7005bbf21d9
Lan Bui <lantbui27@gmail.com>
2021-10-10 16:19:00 -0700

    server.js updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // import sequelize connection
 
+const sequelize = require('./config/connection.js')
 const app = express()
 
 app.use(express.json())
@@ -9,4 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+
+sequelize.sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.log(err))
